da74b45393ede8cbbda9e1b70d18fe15bad208f0
Roberto PM <rpm@Robertos-MBP.home>
2017-12-09 21:43:44 -0500

    command line example with expected output

--- CommandLine.java
+++ CommandLine.java
@@ -4,7 +4,19 @@ Utilizing the command line
 after compiling the program in the command line:
 $ javac CommandLine.java
 run the java program through the command line with:
-java Commandline this is a command line arguement
+java Commandline This is a command one 2 3 and to the 4
+the expected output should be:
+args[0]: This
+args[1]: is
+args[2]: a
+args[3]: command
+args[4]: one
+args[5]: 2
+args[6]: 3
+args[7]: and
+args[8]: to
+args[9]: the
+args[10]: 4
 */
 public class CommandLine{
 	public static void main(String[] args) {
